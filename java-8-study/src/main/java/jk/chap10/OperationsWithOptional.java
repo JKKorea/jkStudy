666e17826479ae7ab2d6a5136452be987bd368e5
JKKorea <jongkun7987@gmail.com>
2021-06-10 08:40:54 +0900

    20210610

--- java-8-study/src/main/java/jk/chap10/OperationsWithOptional.java
+++ java-8-study/src/main/java/jk/chap10/OperationsWithOptional.java
@@ -12,7 +12,13 @@ public class OperationsWithOptional {
     public static void main(String... args) {
         System.out.println(max(of(3), of(5)));
         System.out.println(max(empty(), of(5)));
-        System.out.println(empty().get());
+//        System.out.println(empty().get());
+
+        List<Integer> integers = Arrays.asList(1,2,3);
+        // 구조화된(size가 fix 됨.) 리스트를 반환
+        System.out.println(integers.size());
+
+        integers.add(4); //java.lang.UnsupportedOperationException 발생!
     }
 
     public static final Optional<Integer> max(Optional<Integer> i, Optional<Integer> j) {
--- java-8-study/src/main/java/jk/chap6/CollectorHarness.java
+++ java-8-study/src/main/java/jk/chap6/CollectorHarness.java
@@ -14,6 +14,7 @@ public class CollectorHarness {
         for (int i = 0; i < 10; i++) {
             long start = System.nanoTime();
             primePartitioner.accept(1_000_000);
+            // java7 이후에 숫자를 가독성 있게 보이기 위해서 _를 이용해서 숫자를 나타낼수있음.
             long duration = (System.nanoTime() - start) / 1_000_000;
             if (duration < fastest) fastest = duration;
             System.out.println("done in " + duration);
